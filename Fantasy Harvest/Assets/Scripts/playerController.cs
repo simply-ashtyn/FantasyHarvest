d237d18c5c776594bcc70ed8d3e8057b3ae34aa7
Ashtyn Lane <anlane@student.fullsail.edu>
2024-06-23 12:52:38 -0500

    Spell shooting start

--- Fantasy Harvest/Assets/Scripts/playerController.cs
+++ Fantasy Harvest/Assets/Scripts/playerController.cs
@@ -12,10 +12,15 @@ public class playerController : MonoBehaviour
     [SerializeField] float playerJumpHeight;
     [SerializeField] float gravityValue;
     [SerializeField] float turnSmoothTime;
-  
+
+    [SerializeField] float spellUseRate;
+    [SerializeField] GameObject spell;
+    [SerializeField] int spellRange;
+
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     Vector3 move;
+    bool spellActive;
 
     private void Start()
     {
@@ -23,9 +28,17 @@ public class playerController : MonoBehaviour
     }
 
     void Update()
-    {       
-        if (controller.transform.transform.position.y < 0.01) {
-           groundedPlayer = true;
+    {
+        Movement();
+
+        StartCoroutine(Shoot());
+    }
+
+    void Movement()
+    {
+        if (controller.transform.transform.position.y < 0.01)
+        {
+            groundedPlayer = true;
         }
 
         if (groundedPlayer && playerVelocity.y < 0.5)
@@ -33,7 +46,7 @@ public class playerController : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        move = (transform.right * Input.GetAxis("Horizontal") + transform.forward* Input.GetAxis("Vertical"));
+        move = (transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         // Changes the height position of the player..
@@ -45,5 +58,30 @@ public class playerController : MonoBehaviour
 
         playerVelocity.y -= gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    IEnumerator Shoot()
+    {
+        if (!spellActive && Input.GetButton("Shoot"))
+        {
+            spellActive = true;
+
+            //Did it hit something
+            RaycastHit hit;
+            if(Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, spellRange))
+            {
+                /// DETERMINE ACTION BASED ON ITEM HIT
+                //if (!hit.transform.CompareTag("")) ; // plant, enemy, grass, etc
+                //else  Destroy(hit.transform.gameObject); // used for harvesting/cutting grass
+
+
+                //create object where the player is pointing and is facing
+                Instantiate(spell, hit.point, transform.rotation); // spell.transform.rotation - uses the items rotation not the players (needed for spells?)
+
+            }
+
+            yield return new WaitForSeconds(spellUseRate);
+            spellActive = false;
+        }
     }
 }
