a9195ef9665bfb5070b2271047364788e609c3b2
Ashtyn Lane <97042764+simply-ashtyn@users.noreply.github.com>
2024-06-23 21:13:06 -0500

    edit level

--- Fantasy Harvest/Assets/Scripts/AI/enemyAI.cs
+++ Fantasy Harvest/Assets/Scripts/AI/enemyAI.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour, IDamageable
 {
+    [Header("----Enemy Stats----")]
+    [SerializeField] NavMeshAgent agent;
     [SerializeField] int enemyHP;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,7 @@ public class enemyAI : MonoBehaviour, IDamageable
     // Update is called once per frame
     void Update()
     {
-        
+        //agent.SetDestination(gameManager.instance.player.transform.position);
     }
 
     public void TakeDamage(int dmg)
